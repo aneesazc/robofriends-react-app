0cb5f9d18ab3c7a4378c3d0514b9a884c6e154fd
aneesazc <anees.azc@gmail.com>
2023-07-08 23:05:03 +0100

    Change card size again

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const { name, email, id } = props;
     return (
         <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="robots" src={`https://robohash.org/${id}?size=300x300`} />
+            <img alt="robots" src={`https://robohash.org/${id}?size=250x250`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
